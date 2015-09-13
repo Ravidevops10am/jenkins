608d1830e7f38efb623b04761a3eb4e994c104a0
Vincent Caboara <vcaboara@gmail.com>
2015-09-12 21:55:47 -0700

    Add config.xml. setup.py prints Jenkins version and plugins list.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python -u
 
-print "Hello, World"
+import jenkins
+
+server = jenkins.Jenkins("http://localhost:8080", username="jenkins",
+        password="LeeroyJenkins!")
+version = server.get_version()
+print "Jenkins version %s" % version
+
+plugins = server.get_plugins_info()
+print "Plugins:"
+for p in plugins:
+    print "\t%s" % p["shortName"]
