c64a73509ad82326aadb7d325b992c5c7968bd13
Vincent Caboara <vcaboara@gmail.com>
2015-09-15 17:16:37 -0700

    Use jenkins.cfg for list of required plugins. setup.py prints list of installed plugins, and reports plugins that need to be installed.

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python -u
 
 import jenkins
+from ConfigParser import SafeConfigParser
 
 server = jenkins.Jenkins("http://localhost:8080", username="jenkins",
         password="LeeroyJenkins!")
 version = server.get_version()
 print "Jenkins version %s" % version
 
-plugins = server.get_plugins_info()
-print "Plugins:"
-for p in plugins:
-    print "\t%s" % p["shortName"]
+plugins = []
+print "Installed Plugins:"
+for p in server.get_plugins_info():
+    print "\t%s: %s" % (p["shortName"], p["version"])
+    plugins.append((p["shortName"], p["version"]))
+
+conf = SafeConfigParser()
+conf.read("jenkins.cfg")
+
+for p in conf.items("Plugins"):
+    if p not in plugins:
+        print("Need to install %s ver %s" % (p[0], p[1]))
