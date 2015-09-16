a2bed8256a80bcf06ea8f5791a4ea0ca812cf77d
Vincent Caboara <vcaboara@gmail.com>
2015-09-15 17:33:55 -0700

    setup.py now installs required plugins (via curl -X POST), which will require a restart. Using API Token for password.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python -u
 
+import os
 import jenkins
 from ConfigParser import SafeConfigParser
 
+def install_plugin(name, version):
+    os.system("curl -X POST -d '<jenkins><install plugin=\"%s@%s\" /></jenkins>' --header 'Content-Type: text/xml' http://jenkins:17fd353962ec135cdfd17d85e142605f@localhost:8080/pluginManager/installNecessaryPlugins" % (name, version))
+
 server = jenkins.Jenkins("http://localhost:8080", username="jenkins",
-        password="LeeroyJenkins!")
+        password="17fd353962ec135cdfd17d85e142605f")
 version = server.get_version()
 print "Jenkins version %s" % version
 
@@ -18,5 +22,7 @@ conf = SafeConfigParser()
 conf.read("jenkins.cfg")
 
 for p in conf.items("Plugins"):
+    # XXX Need better logic for newer versions of plugins
     if p not in plugins:
         print("Need to install %s ver %s" % (p[0], p[1]))
+        install_plugin(*p)
